a1fcc682e6c03ac298b9d6cc843546e62d4fb34b
Muhammad Arham <mianarham3060@gmail.com>
2026-09-13 01:44:13 +0500

    feat: implement todo creation

---
+++ src/App.test.tsx
@@ -0,0 +1,45 @@
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import App from './App'
+
+describe('App', () => {
+  it('allows a user to add a todo from text input', async () => {
+    const user = userEvent.setup()
+
+    render(<App />)
+
+    const input = screen.getByLabelText(/todo text/i)
+    await user.type(input, 'Buy milk')
+    await user.click(screen.getByRole('button', { name: /add todo/i }))
+
+    expect(screen.getByText('Buy milk')).toBeInTheDocument()
+  })
+
+  it('rejects empty todo text', async () => {
+    const user = userEvent.setup()
+
+    render(<App />)
+
+    const input = screen.getByLabelText(/todo text/i)
+    await user.clear(input)
+    await user.click(screen.getByRole('button', { name: /add todo/i }))
+
+    expect(screen.queryByText(/buy milk|wash dishes/i)).not.toBeInTheDocument()
+    expect(screen.getByText(/please enter a todo/i)).toBeInTheDocument()
+  })
+
+  it('adds multiple todos to the list', async () => {
+    const user = userEvent.setup()
+
+    render(<App />)
+
+    const input = screen.getByLabelText(/todo text/i)
+    await user.type(input, 'Buy milk')
+    await user.click(screen.getByRole('button', { name: /add todo/i }))
+    await user.type(input, 'Wash dishes')
+    await user.click(screen.getByRole('button', { name: /add todo/i }))
+
+    expect(screen.getByText('Buy milk')).toBeInTheDocument()
+    expect(screen.getByText('Wash dishes')).toBeInTheDocument()
+  })
+})
